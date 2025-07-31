364df4d6c6142fce9dc05e892e0639775775a7eb
Frank <heguer76@gmail.com>
2025-07-31 14:19:45 -0600

    6th fix types

--- README-Github.md
+++ README-Github.md
@@ -17,7 +17,7 @@ git push -u origin main
 git branch
 git checkout main
 git add .
-git commit -m "third fix types"
+git commit -m "6th fix types"
 git push -u origin main
 ```
 
--- actions/orders/createOrderAction.ts
+++ actions/orders/createOrderAction.ts
@@ -1,10 +1,111 @@
+// 'use server'
+// import { auth } from '@/auth'
+// import { prisma } from '@/lib/sample-data/db/prisma'
+// import { isRedirectError } from 'next/dist/client/components/redirect-error'
+// import { getMyCartAction } from '../cart/getMyCartAction'
+// import { insertOrderSchema } from '@/zod-schema-validator/OrderSChema'
+// import { CartItemType } from '@/types'
+
+// export async function createOrderAction() {
+//   try {
+//     const session = await auth()
+//     if (!session) throw new Error('User is not authenticated')
+
+//     const currentUser = await prisma.user.findFirst({
+//       where: { id: session?.user?.id },
+//     })
+//     if (!currentUser) throw new Error('User Not found')
+
+//     const cart = await getMyCartAction()
+
+//     if (!cart || cart.items.length === 0) {
+//       console.log('wehy the fuck are we redirecting form here?')
+
+//       return {
+//         success: false,
+//         message: 'Your cart is empty',
+//         redirectTo: '/cart',
+//       }
+//     }
+//     if (!currentUser.address) {
+//       return {
+//         success: false,
+//         message: 'Not shipping Adddress',
+//         redirectTo: '/shipping-address',
+//       }
+//     }
+//     if (!currentUser.paymentMethod) {
+//       return {
+//         success: false,
+//         message: 'Not payment method selected',
+//         redirectTo: '/payment-method',
+//       }
+//     }
+
+//     //create /setuporder object
+//     const order = insertOrderSchema.parse({
+//       userId: currentUser.id,
+//       shippingAddress: currentUser.address,
+//       paymentMethod: currentUser.paymentMethod,
+//       itemsPrice: cart.itemsPrice,
+//       shippingPrice: cart.shippingPrice,
+//       taxPrice: cart.taxPrice,
+//       totalPrice: cart.totalPrice,
+//     })
+
+//     //create a transation first to be able to create  the order and order items in database
+//     const insertedOrderId = await prisma.$transaction(async (tx) => {
+//       //ceate order
+//       const insertedOrder = await tx.order.create({ data: order })
+//       //create order items from the cartItems
+//       for (const item of cart.items as CartItemType[]) {
+//         await tx.orderItem.create({
+//           data: {
+//             ...item,
+//             price: item.price,
+//             orderId: insertedOrder.id,
+//           },
+//         })
+//       }
+//       //update cart
+//       await tx.cart.update({
+//         where: { id: cart.id },
+//         data: {
+//           items: [],
+//           totalPrice: 0,
+//           taxPrice: 0,
+//           shippingPrice: 0,
+//           itemsPrice: 0,
+//         },
+//       })
+
+//       return insertedOrder.id
+//     })
+//     if (!insertedOrderId)
+//       throw new Error('Order not created, error lin3 83 createOrderAction')
+//     return {
+//       success: true,
+//       message: 'Order created succesfuuly',
+//       redirectTo: `/order/${insertedOrderId}`,
+//     }
+//   } catch (error) {
+//     if (isRedirectError(error)) throw error
+//     return {
+//       success: false,
+//       message: 'No order was creted, catch error',
+//       redirectTo: '/catch-error',
+//     }
+//   }
+// }
 'use server'
+
 import { auth } from '@/auth'
 import { prisma } from '@/lib/sample-data/db/prisma'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { getMyCartAction } from '../cart/getMyCartAction'
 import { insertOrderSchema } from '@/zod-schema-validator/OrderSChema'
 import { CartItemType } from '@/types'
+import { Prisma } from '@prisma/client'
 
 export async function createOrderAction() {
   try {
@@ -19,30 +120,29 @@ export async function createOrderAction() {
     const cart = await getMyCartAction()
 
     if (!cart || cart.items.length === 0) {
-      console.log('wehy the fuck are we redirecting form here?')
-
       return {
         success: false,
         message: 'Your cart is empty',
         redirectTo: '/cart',
       }
     }
+
     if (!currentUser.address) {
       return {
         success: false,
-        message: 'Not shipping Adddress',
+        message: 'No shipping address',
         redirectTo: '/shipping-address',
       }
     }
+
     if (!currentUser.paymentMethod) {
       return {
         success: false,
-        message: 'Not payment method selected',
+        message: 'No payment method selected',
         redirectTo: '/payment-method',
       }
     }
 
-    //create /setuporder object
     const order = insertOrderSchema.parse({
       userId: currentUser.id,
       shippingAddress: currentUser.address,
@@ -53,46 +153,49 @@ export async function createOrderAction() {
       totalPrice: cart.totalPrice,
     })
 
-    //create a transation first to be able to create  the order and order items in database
-    const insertedOrderId = await prisma.$transaction(async (tx) => {
-      //ceate order
-      const insertedOrder = await tx.order.create({ data: order })
-      //create order items from the cartItems
-      for (const item of cart.items as CartItemType[]) {
-        await tx.orderItem.create({
+    const insertedOrderId = await prisma.$transaction(
+      // lets leave it the type as ay for now. it seems we need a newer version of prisma
+      async (tx: any) => {
+        const insertedOrder = await tx.order.create({ data: order })
+
+        for (const item of cart.items as CartItemType[]) {
+          await tx.orderItem.create({
+            data: {
+              ...item,
+              price: item.price,
+              orderId: insertedOrder.id,
+            },
+          })
+        }
+
+        await tx.cart.update({
+          where: { id: cart.id },
           data: {
-            ...item,
-            price: item.price,
-            orderId: insertedOrder.id,
+            items: [],
+            totalPrice: 0,
+            taxPrice: 0,
+            shippingPrice: 0,
+            itemsPrice: 0,
           },
         })
-      }
-      //update cart
-      await tx.cart.update({
-        where: { id: cart.id },
-        data: {
-          items: [],
-          totalPrice: 0,
-          taxPrice: 0,
-          shippingPrice: 0,
-          itemsPrice: 0,
-        },
-      })
 
-      return insertedOrder.id
-    })
+        return insertedOrder.id
+      }
+    )
+
     if (!insertedOrderId)
-      throw new Error('Order not created, error lin3 83 createOrderAction')
+      throw new Error('Order not created, error at createOrderAction')
+
     return {
       success: true,
-      message: 'Order created succesfuuly',
+      message: 'Order created successfully',
       redirectTo: `/order/${insertedOrderId}`,
     }
   } catch (error) {
     if (isRedirectError(error)) throw error
     return {
       success: false,
-      message: 'No order was creted, catch error',
+      message: 'No order was created, caught an error',
       redirectTo: '/catch-error',
     }
   }
